b0e26ce27c5e45fc3adcda758551dd51cf70daf2
Serhii Komarychev <komarichev@gmail.com>
2018-01-19 14:00:09 +0200

    Learn Vue Component

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
-$(document).ready(function () {
-    /**
-     * Add button click handler
-     */
-    function onAdd() {
-        var ul, li, $li, $label, $div, value;
-        value = document.querySelector('.js-new-item').value;
-        
-        //validate against empty values
-        if (value === '') {
-            return;
-        }
-        ul = document.querySelector('ul');
-        $li = $('<li>').appendTo(ul);
-        $div = $('<div>')
-            .addClass('checkbox')
-            .appendTo($li);
-        $label = $('<label>').appendTo($div);
-        $('<input>')
-            .attr('type', 'checkbox')
-            .addClass('item')
-            .attr('name', 'list')
-            .click(toggleRemoved)
-            .appendTo($label);
-        $label
-            .append(value);
-        document.querySelector('.js-new-item').value=''
+/*jshint esversion: 6 */
+
+var data = {
+  items: [
+    { text: "Bananas", checked: false },
+    { text: "Apples", checked: false },
+    { text: "Kiwi", checked: false },
+    { text: "Pineapple", checked: true }
+  ],
+  title: "Список Сотрудников AW",
+  newItem: ""
+};
+
+/* 
+Declaring Components 
+*/
+var ItemsComponent = Vue.extend({
+    data: function () {
+        return data;
+    },
+    template: `
+        <ul>
+            <li v-for="item in items"
+                :class="{'removed': item.checked}">
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" v-model="item.checked">
+                    {{item.text}}
+                </label>
+            </div>
+            </li>
+        </ul>
+    `
+});
+var ChangeTitleComponent = Vue.extend({
+  data: function() {
+    return data;
+  },
+  template: `<input v-model="title">`
+});
+var AddItemComponent = Vue.extend({
+  data: function() {
+    return data;
+  },
+  methods: {
+    addItem: function() {
+      var text;
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
     }
-    /**
-     * Checkbox click handler -
-     * toggles class removed on li parent element
-     * @param ev
-     */
-    function toggleRemoved(ev) {
-        var $el;
-        $el = $(ev.currentTarget);
-        $el.closest('li').toggleClass('removed');
-    }
-    $('.js-add').click(onAdd);
-    $('.js-item').click(toggleRemoved);
+  },
+  template: `
+         <div class="input-group">
+            <input @keyup.enter="addItem" placeholder="add shopping list item" type="text" class="js-new-item form-control">
+            <span class="input-group-btn">
+                <button @click="addItem" class="js-add btn btn-default" type="button">Add!</button>
+            </span>
+        </div>
+    `
+});
+/* 
+Registering component
+ */
+Vue.component('items-component', ItemsComponent);
+Vue.component('change-title-comonent', ChangeTitleComponent);
+Vue.component('add-item-component', AddItemComponent);
+/* 
+Instanting a Vue instanse
+ */
+new Vue({
+    el: '#app',
+    data: data
 });
